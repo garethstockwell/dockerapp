1a603add418d6c4c4b0e88dd01e19728f0f9b500
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2016-03-14 15:38:13 +0000

    shell: fix command not found script

--- shell/shrc.sh
+++ shell/shrc.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+if [ -n "$(declare -f command_not_found_handle)" ]; then
+    if [ -z $PATH_DOCKERAPP ]; then
+        eval "$(echo "_command_not_found_handle()"; declare -f command_not_found_handle | tail -n +2)"
+    fi
+fi
+
 PATH_DOCKERAPP=$(builtin cd "$(dirname ${BASH_SOURCE[0]:-$0})/.." && pwd)
 
 export PATH=$PATH:$PATH_DOCKERAPP/bin
 
 function command_not_found_handle
 {
-    # Check if there is a container image with that name
-    if ! docker inspect --format '{{ .Author }}' "$1" >&/dev/null; then
-        echo "$0: $1: command not found"
+    if ! docker inspect --format '{{ .Name }}' "$1" >&/dev/null; then
+        _command_not_found_handle $1
         return
     fi
 
     # Check that it's really the name of the image, not a prefix
     if docker inspect --format '{{ .Id }}' "$1" | grep -q "^$1" ;then
-        echo "$0: $1: command not found"
+        _command_not_found_handle $1
         return
     fi
 
